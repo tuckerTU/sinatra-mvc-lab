49d327d8f0fbce7f98b00963ce5cd019363758bb
tuckerTU <tuckerishere@gmail.com>
2021-01-31 10:36:59 -0700

    commit tree 4b14ae6e3e8ef5021fd655fa95f5c8b8420c9406

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
+require 'pry'
 
 class App < Sinatra::Base
-end
+
+    get '/' do
+      erb :user_input
+    end 
+    
+    post '/piglatinize' do 
+    
+        phrase = PigLatinizer.new
+        @user_input = phrase.to_pig_latin(params[:user_phrase])
+        erb :piglatinize
+    
+    end 
+    
+    end
